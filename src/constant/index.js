17930acf9d20602a458fdfb5dbfa8ff1b89dfa8e
muratder88 <murat_fakir62@hotmail.com>
2018-10-05 02:07:20 +0300

    first commit

---
+++ src/constant/index.js
@@ -0,0 +1,9 @@
+
+const types = {
+  INCREMENT_REQUESTED: 'counter/INCREMENT_REQUESTED',
+  INCREMENT: 'counter/INCREMENT',
+  DECREMENT_REQUESTED: 'counter/DECREMENT_REQUESTED',
+  DECREMENT:'counter/DECREMENT',
+}
+
+export default types;
